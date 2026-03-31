64b610fb10ab56ce9228f91ee29b0e46ac3598dc
Nicolas <nicolasmader001@gmail.com>
2026-03-31 10:34:28 +0200

    Add database user Max

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#datenbank übung
+#datenbankübungen
 
 import sqlite3
 
@@ -6,12 +6,22 @@ import sqlite3
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
-#tabelle erstellen
-c.execute("""
-create table if not exists users (
-    id integer primary key autoincrement,   
-    name text not null """)               #id integer primary key autoincrement =wird automatisch hochgezählt
-                                          #name text not null = Name, darf nicht leer sein
-#speichern/schließen
+# Tabelle erstellen                             #c.execute=Abfrage ausführen
+c.execute("""                                       
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL)
+""")
+
+#user erstellen
+c.execute("INSERT INTO users (name) VALUES (?)",("Max",))   #Befehl, um einen neuen Datensatz in die Tabelle einzufügen
+
+#speichern
 conn.commit()    #Speichert (bestätigt) alle Änderungen dauerhaft in der Datenbank
+
+#überprüfen ob user da ist
+c.execute("SELECT * FROM users")
+print(c.fetchall())        #c.fetchall=alle Ergebnisse abrufen
+
+# schließen
 conn.close()      #schließen
