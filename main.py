7c088c69dbf4141899d84b3aeaac35c5ca2289e8
Nicolas <nicolasmader001@gmail.com>
2026-03-31 10:22:31 +0200

    Add database table

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 
 import sqlite3
 
+#verbindung aufbauen
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
+#tabelle erstellen
+c.execute("""
+create table if not exists users (
+    id integer primary key autoincrement,   
+    name text not null """)               #id integer primary key autoincrement =wird automatisch hochgezählt
+                                          #name text not null = Name, darf nicht leer sein
+#speichern/schließen
 conn.commit()    #Speichert (bestätigt) alle Änderungen dauerhaft in der Datenbank
 conn.close()      #schließen
