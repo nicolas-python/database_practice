25f3fa1052fbc346bd4121a637eefae3354ccceb
Nicolas <nicolasmader001@gmail.com>
2026-04-01 11:48:08 +0200

    Add filtering with LIKE

--- main.py
+++ main.py
@@ -16,16 +16,20 @@ CREATE TABLE IF NOT EXISTS users (
 #user erstellen
 users = [("Nico",),("Timo",),("Thomas",)]
 c.executemany("INSERT INTO users (name) VALUES (?)",users)   #executemany für jeden Tupel ein insert
-                                                             #?= platzhalter
+                                                             #?= platzhalter für parameter
 #speichern
 conn.commit()    #Speichert (bestätigt) alle Änderungen dauerhaft in der Datenbank
 
 #user löschen
-c.execute("DELETE FROM users WHERE name = ?", ("Timo",))
+#c.execute("DELETE FROM users WHERE name = ?", ("Timo",))
 
 #namen ändern
 c.execute("UPDATE users SET name = ? WHERE name = ?", ("Niklas", "Nico"))    #ohne where werden alle user geändert
 conn.commit()
+
+#daten gezielt abfragen
+c.execute("SELECT * FROM users WHERE name LIKE ?", ("%i%",))    #% Platzhalter für abfragen(beliebige Zeichen 0 oder mehr)
+print(c.fetchall())                                                           #_ Platzhalter für ein einzelnes Zeichen
 
 #überprüfen ob user da ist
 c.execute("SELECT * FROM users")        #c.execute = Befehl, um einen neuen Datensatz in die Tabelle einzufügen
