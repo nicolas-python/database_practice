748d3bba687cf5e83860ff933b6dd47a0ba99e47
Nicolas <nicolasmader001@gmail.com>
2026-04-01 11:56:31 +0200

    Add sorting and delete functionality

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ CREATE TABLE IF NOT EXISTS users (
     name TEXT NOT NULL)
 """)
 
+#alle löschen (sauberer start)
+c.execute("DELETE FROM users")
+conn.commit()
+
 #user erstellen
 users = [("Nico",),("Timo",),("Thomas",)]
 c.executemany("INSERT INTO users (name) VALUES (?)",users)   #executemany für jeden Tupel ein insert
@@ -20,24 +24,27 @@ c.executemany("INSERT INTO users (name) VALUES (?)",users)   #executemany für j
 #speichern
 conn.commit()    #Speichert (bestätigt) alle Änderungen dauerhaft in der Datenbank
 
-#user löschen
-#c.execute("DELETE FROM users WHERE name = ?", ("Timo",))
-
 #namen ändern
 c.execute("UPDATE users SET name = ? WHERE name = ?", ("Niklas", "Nico"))    #ohne where werden alle user geändert
 conn.commit()
 
 #daten gezielt abfragen
+print("Filter: Namen mit 'i'")
 c.execute("SELECT * FROM users WHERE name LIKE ?", ("%i%",))    #% Platzhalter für abfragen(beliebige Zeichen 0 oder mehr)
 print(c.fetchall())                                                           #_ Platzhalter für ein einzelnes Zeichen
+
+# Daten sortieren
+print("\nAlle User alphabetisch sortiert")
+c.execute("SELECT * FROM users ORDER BY name")
+print(c.fetchall())
+
+#user löschen
+c.execute("DELETE FROM users WHERE name = ?", ("Thomas",))
 
 #überprüfen ob user da ist
 c.execute("SELECT * FROM users")        #c.execute = Befehl, um einen neuen Datensatz in die Tabelle einzufügen
 print(c.fetchall())        #c.fetchall=alle Ergebnisse abrufen
 
-#alle löschen
-#c.execute("DELETE FROM users")
-#conn.commit()
 
 # schließen
 conn.close()      #schließen
